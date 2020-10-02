d0ae73093ce77cbcdb543235fb695d2b5ef7bd01
banao <tatitachi870@gmail.com>
2020-10-02 21:53:48 +0900

    サブアクティビティの追加

---
+++ app/src/main/java/com/example/hskapp/LevelActivity.java
@@ -0,0 +1,25 @@
+package com.example.hskapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level);
+
+        // 戻るボタン（Activityの終了）
+        Button returnButton = findViewById(R.id.return_button);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/hskapp/MainActivity.java
+++ app/src/main/java/com/example/hskapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.hskapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button levelButton = findViewById(R.id.button_level);
+        levelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toLevelIntent = new Intent(getApplication(), LevelActivity.class);
+                startActivity(toLevelIntent);
+            }
+        });
+
+        Button myvocButton = findViewById(R.id.button_myvoc);
+        myvocButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toMyvocIntent = new Intent(getApplication(), MyvocActivity.class);
+                startActivity(toMyvocIntent);
+            }
+        });
     }
 }
